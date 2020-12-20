98ee64680e02204413180352372c76822b7d8334
GersonUmanzor <gbumanzor@outlook.com>
2020-12-19 22:26:18 -0600

    Add pipe operator.

---
+++ pipe-operator.php
@@ -0,0 +1,31 @@
+<?php
+
+class ChargeRejected extends Exception {
+
+}
+
+class NotEnoughFunds extends Exception {
+
+}
+
+class User {
+
+    public function subscribe()
+    {
+//        var_dump('subscribing');
+        throw new ChargeRejected('Error');
+    }
+}
+
+function flash($message)
+{
+    var_dump($message);
+}
+
+try
+{
+    (new User)->subscribe();
+} catch (ChargeRejected | NotEnoughFunds $e)
+{
+    flash('Failed');
+}
